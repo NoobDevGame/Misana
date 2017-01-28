22d9441855130d3aff9873def716f491c6b576a4
Lassi <csharplassi@posteo.de>
2017-01-28 16:49:27 +0100

    Debug if entfernt

--- Misana.Core/EffectApplicator.cs
+++ Misana.Core/EffectApplicator.cs
@@ -103,12 +103,6 @@ namespace Misana.Core
 
         private void OnCreateProjectileEffect(OnCreateProjectileEffectMessage message, MessageHeader header, NetworkClient client)
         {
-
-            if (simulation.Mode == SimulationMode.Server)
-            {
-
-            }
-
             var id = Interlocked.Increment(ref _dummyId) % ushort.MaxValue + ushort.MaxValue;
             EntityBuilder builder = new EntityBuilder();
             builder.Add<MotionComponent>(x => x.Move = message.move )
